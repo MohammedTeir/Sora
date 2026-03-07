e5b40ddebd6a15a3f757b6e60e388a7dd6fbf41d
mahmoud-teir <mahmoudteirbusiness@gmail.com>
2026-03-07 20:20:18 +0200

    Update MigrateSourceScreen.kt

--- app/src/main/java/eu/kanade/presentation/browse/MigrateSourceScreen.kt
+++ app/src/main/java/eu/kanade/presentation/browse/MigrateSourceScreen.kt
@@ -37,6 +37,11 @@ import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import androidx.compose.foundation.clickable
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.text.style.TextOverflow
 import eu.kanade.domain.source.interactor.SetMigrateSorting
 import eu.kanade.presentation.browse.components.SourceIcon
 import eu.kanade.tachiyomi.ui.browse.migration.sources.MigrateSourceScreenModel
